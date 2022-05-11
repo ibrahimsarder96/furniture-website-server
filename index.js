378cfdb6deddcb9858ac3e404e2653635d26035c
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-05-11 16:15:01 +0600

    added jwt code

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: 'unauthorized access'});
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err, decoded) =>{
+    if(err){
+      return res.status(403).send({message: 'Forbidden access'});
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qbosk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -44,16 +58,36 @@ async function run(){
       res.send(product);
     });
     // POST
-    app.get('/product', async(req, res) =>{
+    app.get('/product', verifyJWT, async(req, res) =>{
+      const decodedEmail = req.decoded.email;
       const email = req.query.email;
+     if(email === decodedEmail){
       const query = {email: email};
       const cursor = productCollection.find(query);
-      const products = await cursor.toArray();
-      res.send(products);
+      const orders = await cursor.toArray();
+      res.send(orders);
+     }
+     else{
+       res.status(403).send({message: 'Forbidden access'})
+     }
     })
     app.post('/product', async(req, res) =>{
       const newProduct = req.body;
       const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
+    //Update Product
+    app.put('/product/:id', async(req, res)=>{
+      // const id = req.params.id;
+      // const updateQuantity = req.body;
+      // const filter = {_id: ObjectId(id)};
+      // const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          quantity: updateQuantity.quantity
+        }
+      };
+      const result = await productCollection.updateOne(filter, updateDoc, options);
       res.send(result);
     });
     //DELETE
